9daeac98a180ed55fff78fa54acecfb7c2b24489
Nair_Ishani <ishanimnair@gmail.com>
2026-05-27 01:56:07 +0530

    Day 1 commits

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -7,5 +7,37 @@ use Illuminate\Http\Request;
 
 class LeadController extends Controller
 {
-    //
+    public function index(){
+        $leads=Lead::all();
+        return view('leads.index'. compact('leads'));
+    }
+
+    public function create(){
+        return view('leads.create')''
+    }
+
+    public function store(Request $request){
+        $request->validate(['name'=>'required','email'=>'required|email']);
+        Lead::create($request->all());
+        return redirect()->route('leads.index')->with('success','Lead created succesfully!');
+    }
+    
+    public function show(Lead $lead){
+        return view('leads.show',compact('lead'));
+    }
+
+    public function edit(Lead $lead){
+        return view('leads.edit',compact('lead'));
+    }
+
+    public function store(Request $request, Lead $lead){
+        $request->validate(['name'=>'required','email'=>'required|email']);
+        $lead->upadte($request->all());
+        return redirect()->route('leads.index')->with('success','Lead updated succesfully!');
+    }
+
+    public function destroy(Lead $lead){
+        $lead->delete();
+         return redirect()->route('leads.index')->with('success','Lead deleted succesfully!');
+    }
 }
